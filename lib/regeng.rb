702466ed269c1a06215b2b206d8d7bd2f8bb149f
LucHighwalker <luc.highwalker@gmail.com>
2018-12-10 11:16:28 -0800

    - Made strings less tollerable.

--- lib/regeng.rb
+++ lib/regeng.rb
@@ -9,25 +9,25 @@ module Regeng
     middle = ''
     ending = ''
 
-    start = process_start_of(string) if string =~ /( at ){1}/
+    start = process_at(string) if string =~ /( at ){1}/
     middle = process_any(string) if string =~ /(any ){1}/
 
     expression = "#{start}#{middle}#{ending}"
     Regexp.new expression
   end
 
-  def self.process_start_of(string)
+  def self.process_at(string)
     result = ''
-    if string =~ /(start of){1}/
-      if string =~ /(line){1}/
+    if string =~ /(at start of ){1}/
+      if string =~ /( line){1}/
         result = '^'
-      elsif string =~ /(string){1}/
+      elsif string =~ /( string){1}/
         result = '\A'
       end
-    elsif string =~ /(end of){1}/
-      if string =~ /(line){1}/
+    elsif string =~ /(at end of ){1}/
+      if string =~ /( line){1}/
         result = '$'
-      elsif string =~ /(string){1}/
+      elsif string =~ /( string){1}/
         result = '\z'
       end
     end
@@ -36,21 +36,21 @@ module Regeng
 
   def self.process_any(string)
     result = ''
-    if string =~ /(character){1}/
-      if string =~ /([a-z]-[a-z])/i
-        character_mod = string.match(/([a-z]-[a-z])/i)
-        character_mod = "^#{character_mod}" if string =~ /(except){1}/
+    if string =~ /( character){1}/
+      if string =~ /( [a-z]-[a-z])/i
+        character_mod = string.match(/( [a-z]-[a-z])/i)
+        character_mod = "^#{character_mod}" if string =~ /( except ){1}/
         result = "[#{character_mod}]"
-      elsif string =~ /(uppercase){1}/
+      elsif string =~ /( uppercase ){1}/
         result = '[A-Z]'
-      elsif string =~ /(lowercase){1}/
+      elsif string =~ /( lowercase ){1}/
         result = '[a-z]'
       else
         result = '[A-Za-z]'
       end
-    elsif string =~ /(digit){1}/
-      if string =~ /([0-9]+-[0-9]+){1}/
-        digit_mod = string.match(/([0-9]+-[0-9]+){1}/)
+    elsif string =~ /( digit){1}/
+      if string =~ /( [0-9]+-[0-9]+){1}/
+        digit_mod = string.match(/( [0-9]+-[0-9]+){1}/)
         digit_mod = "^#{digit_mod}" if string =~ /(except){1}/
         result = "[#{digit_mod}]"
       else
