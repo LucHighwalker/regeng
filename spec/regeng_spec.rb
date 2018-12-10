211c0d69361fd44a89f8286b4533f9be7c6a2508
LucHighwalker <luc.highwalker@gmail.com>
2018-12-10 12:26:45 -0800

    - Created tests for next milestone.

--- spec/regeng_spec.rb
+++ spec/regeng_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe Regeng do
   it 'has a version number' do
     expect(Regeng::VERSION).not_to be nil
@@ -17,19 +19,51 @@ RSpec.describe Regeng do
       expect(Regeng.expression(string)).to eq(/[A-Z]/)
     end
     it 'should return any characters with modifier' do
+      string = 'any character b through w'
+      expect(Regeng.expression(string)).to eq(/[b-w]/)
+    end
+    it 'should return any characters with modifier' do
       string = 'any character b-w'
       expect(Regeng.expression(string)).to eq(/[b-w]/)
     end
+    it 'should return any characters with modifier' do
+      string = 'any character between b and w'
+      expect(Regeng.expression(string)).to eq(/[b-w]/)
+    end
     it 'should return any characters except modifier' do
-      string = 'any character except b-w'
+      string = 'any character except b through w'
       expect(Regeng.expression(string)).to eq(/[^b-w]/)
+    end
+    it 'should return any characters except specific modifier' do
+      string = 'any character except b and w'
+      expect(Regeng.expression(string)).to eq(/[^bw]/)
     end
   end
 
   context '#expression_digits' do
-    it 'should return any digit' do 
+    it 'should return any digit' do
       string = 'any digit'
       expect(Regeng.expression(string)).to eq(/[0-9]+/)
+    end
+    it 'should return any digit between 5 and 9' do
+      string = 'any digit between 5 and 9'
+      expect(Regeng.expression(string)).to eq(/[0-9]+/)
+    end
+    it 'should return any digit between 5 and 9 with dash' do
+      string = 'any digit 5-9'
+      expect(Regeng.expression(string)).to eq(/[0-9]+/)
+    end
+    it 'should return any digit between 20 and 60' do
+      string = 'any digit between 20 and 60'
+      expect(Regeng.expression(string)).to eq(/(2[0-9])|(3[0-9])|(4[0-9])|(5[0-9])|(60)/)
+    end
+    it 'should return any digit between 400 and 600' do
+      string = 'any digit between 400 and 600'
+      expect(Regeng.expression(string)).to eq(/(4[0-9][0-9])|(5[0-9][0-9])|(600)/)
+    end
+    it 'should return any digit between 7000 and 9000' do
+      string = 'any digit between 7000 and 9000'
+      expect(Regeng.expression(string)).to eq(/(7[0-9][0-9][0-9])|(8[0-9][0-9][0-9])|(9000)/)
     end
   end
 
