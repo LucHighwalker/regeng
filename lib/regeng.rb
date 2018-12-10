6085521f46a5289d1b346c5112f5af3c3887183d
LucHighwalker <luc.highwalker@gmail.com>
2018-12-09 17:07:22 -0800

    - Copied over initial prototype.

--- lib/regeng.rb
+++ lib/regeng.rb
@@ -1,6 +1,52 @@
-require "regeng/version"
+require 'regeng/version'
 
+# A gem which creates regular expressions using plain english.
 module Regeng
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.get_expression(string)
+    array = string.split(' and ')
+    start = ''
+    middle = ''
+    ending = ''
+    if string =~ /(any){1}/
+      if string =~ /(character){1}/
+        if string =~ /[a-z]-[a-z]/i
+          character_mod = string.match(/[a-z]-[a-z]/i)
+          character_mod = "^#{character_mod}" if string =~ /(except){1}/
+          middle = "#{middle}[#{character_mod}]"
+        elsif string =~ /(uppercase){1}/
+          middle = "#{middle}[A-Z]"
+        elsif string =~ /(lowercase){1}/
+          middle = "#{middle}[a-z]"
+        else
+          middle = "#{middle}[A-Za-z]"
+        end
+      elsif string =~ /(digits){1}/
+        if string =~ /([0-9]+-[0-9]+){1}/
+          character_mod = string.match(/([0-9]+-[0-9]+){1}/)
+          character_mod = "^#{character_mod}" if string =~ /(except){1}/
+          middle = "#{middle}[#{character_mod}]"
+        else
+          middle = "#{middle}[0-9]"
+        end
+      end
+      if string =~ /(at){1}/
+        if string =~ /(start of){1}/
+          if string =~ /(line)?/
+            start = "#{start}^"
+          elsif string =~ /(string){1}/
+            start = "#{start}\\A"
+          end
+        elsif string =~ /(end of){1}/
+          if string =~ /(line){1}/
+            start = "#{start}$"
+          elsif string =~ /(string){1}/
+            start = "#{start}\\z"
+          end
+        end
+      end
+    end
+    "#{start}#{middle}#{ending}"
+  end
 end
--- spec/regeng_spec.rb
+++ spec/regeng_spec.rb
@@ -3,7 +3,35 @@ RSpec.describe Regeng do
     expect(Regeng::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context '#get_expression' do
+    it 'should return any characters' do
+      string = 'any characters'
+      expect(Regeng.get_expression(string)).to eq '[A-Za-z]'
+    end
+    it 'should return any lowercase characters' do
+      string = 'any lowercase characters'
+      expect(Regeng.get_expression(string)).to eq '[a-z]'
+    end
+    it 'should return any uppercase characters' do
+      string = 'any uppercase characters'
+      expect(Regeng.get_expression(string)).to eq '[A-Z]'
+    end
+    it 'should return any characters with modifier' do
+      string = 'any characters b-w'
+      expect(Regeng.get_expression(string)).to eq '[b-w]'
+    end
+    it 'should return any characters except modifier' do
+      string = 'any characters except b-w'
+      expect(Regeng.get_expression(string)).to eq '[^b-w]'
+    end
+
+    it 'should return any digits with modifier' do
+      string = 'any digits 215-420'
+      expect(Regeng.get_expression(string)).to eq '[215-420]'
+    end
+    it 'should return any digits except modifier' do
+      string = 'any digits except 215-420'
+      expect(Regeng.get_expression(string)).to eq '[^215-420]'
+    end
   end
 end
